c3bed1e525ec9a66f9e38bee46e51fb2fca2bbd6
Pravin Khabile <pravin.khabile@leapdev.io>
2020-10-06 21:59:59 +1100

    Initial integration with auth0

--- components/app/LoginButton.js
+++ components/app/LoginButton.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Button } from "reactstrap";
+import Link from "next/link";
 
 const LoginButton = () => {
   return (    
-      <Button
-        color="success"
-        target="_blank"
-        rel="noreferrer noopener"
-        className="my-2 ml-2 btn-pill"        
-      >
-        Login
-      </Button>    
+      // <Button
+      //   color="success"
+      //   target="_blank"
+      //   rel="noreferrer noopener"
+      //   className="my-2 ml-2 btn-pill"        
+      // >
+      //   Login
+      // </Button>    
+      <Link href="/api/login">
+        <a >Login</a>
+      </Link>
   );
 };
 
--- components/app/LogoutButton.js
+++ components/app/LogoutButton.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Button } from "reactstrap";
+import Link from "next/link";
 
 const LogoutButton = () => {  
   return (
-      <Button
-        color="warning"
-        target="_blank"
-        rel="noreferrer noopener"
-        className="my-2 ml-2 btn-pill">
-        Log Out
-      </Button>    
+      // <Button
+      //   color="warning"
+      //   target="_blank"
+      //   rel="noreferrer noopener"
+      //   className="my-2 ml-2 btn-pill">
+      //   Log Out
+      // </Button>    
+      <Link href="/api/logout">
+        <a >Logout</a>
+      </Link>
   );
 };
 
