c9a1ce6f7da8310a54001203bc2164d5032d0bbc
Pravin Khabile <pravin.khabile@leapdev.io>
2020-11-01 13:59:51 +1100

    Login callback changes to put custom fields into the cookie

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -1,10 +1,43 @@
-import auth0 from '../../utils/auth0';
-import axiosClient from '../../lib/apiproxy/axiosClient';
-
+import auth0 from "../../utils/auth0";
+import axiosClient from "../../lib/apiproxy/axiosClient";
+import { CHECK_USER_EXISTENCE } from "../../lib/apiproxy/mutations";
 
 export default async function callback(req, res) {
-  try {    
-    await auth0.handleCallback(req, res, { redirectTo: '/' });    
+  try {
+    await auth0.handleCallback(req, res, {
+      onUserLoaded: async (req, res, session, state) => {
+        try {
+          const dbUserRecordResponse = await axiosClient.PostQuery(
+            CHECK_USER_EXISTENCE,
+            {  user: {
+                  firstName: session.user.given_name,
+                  lastName: session.user.family_name,
+                  email: session.user.email,
+                  userName: session.user.nickname,
+                  socialLoginId: session.user.sub,
+                  socialProfilePicUrl: session.user.picture,
+                }
+            }
+          );
+
+          let userId = null;
+          if (dbUserRecordResponse.data.data.checkUserExistence != null) {
+            userId = dbUserRecordResponse.data.data.checkUserExistence.id;
+          } else {
+            userId = null;
+          }
+          return {
+            ...session,
+            user: {
+              ...session.user,
+              userId: userId,
+            },
+          };
+        } catch (err) {
+          console.log(err);
+        }
+      },
+    });
   } catch (error) {
     console.error(error);
     res.status(error.status || 400).end(error.message);
