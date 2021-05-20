b4d539e89963d6d5b9b0675007b80600965556f0
Pravin <khabilepravin@gmail.com>
2021-05-20 20:28:10 +1000

    Heading changes for the main page

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Landing = (props) => {
   return (
     <LandingLayout user={props.user}>
       <Head>
-        <title>NAPLAN, ICAS and Opportunity Class Style tests for primary level students</title>
+        <title>The ultimate destination to practice NAPLAN, ICAS and OC Style tests</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content="Ultimate destination for students to practice NAPLAN, ICAS and Opportunity Class Style tests" />
       </Head>
