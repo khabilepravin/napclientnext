9cb5c73ba61f329d38f677f5d5935699f1aaaa56
Pravin <khabilepravin@gmail.com>
2021-06-24 20:28:02 +1000

    Fixes for the title of the page in practice mode

--- pages/practice/[studentid]/[testinstanceid].js
+++ pages/practice/[studentid]/[testinstanceid].js
@@ -4,13 +4,24 @@ import Dashboard from "../../../components/layout/Dashboard";
 import { useRouter } from "next/router";
 import axiosClient from "../../../lib/apiproxy/axiosClient";
 import { GET_USER_TEST_INSTANCE } from "../../../lib/apiproxy/queries";
+import Head from "next/head";
 
 import Layout from "../../../components/layout/Layout";
 
 const TestInstance = (props) => {
+  console.log(JSON.stringify(props));
   const router = useRouter();
   return (
     <Layout>
+      <Head>
+        <title>
+          Practice 
+        </title>
+        <meta
+          name="description"
+          content=""
+        />
+      </Head>
       <PracticeTest userTestInstance={props.userTestById} />
     </Layout>
   );
@@ -19,17 +30,17 @@ const TestInstance = (props) => {
 TestInstance.layout = Dashboard;
 
 export async function getServerSideProps(context) {
- const { testinstanceid } = context.query;
- const userTestInstanceResponse = await axiosClient.PostQuery(
-      GET_USER_TEST_INSTANCE,
-      {
-        id: testinstanceid,
-      }
-    );
+  const { testinstanceid } = context.query;
+  const userTestInstanceResponse = await axiosClient.PostQuery(
+    GET_USER_TEST_INSTANCE,
+    {
+      id: testinstanceid,
+    }
+  );
 
-    return {
-      props: userTestInstanceResponse.data,
-    };
+  return {
+    props: userTestInstanceResponse.data,
+  };
 }
 
 export default TestInstance;
